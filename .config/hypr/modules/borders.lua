bc557bc6d3c5248eec1eb4e204a8be16d0eca395
midn8crow <231871152+midn8crow@users.noreply.github.com>
2026-07-29 22:39:57 +0600

    Cleanup + updated 2026-07-29_22:39

--- .config/hypr/modules/borders.lua
+++ .config/hypr/modules/borders.lua
@@ -1,8 +1,8 @@
 hl.config({
     general = {
         col = {
-            active_border   = "rgba(86d1eaee)",
-            inactive_border = "rgba(bfc8ccaa)",
+            active_border   = "rgba(82d3e2ee)",
+            inactive_border = "rgba(bfc8caaa)",
         },
     },
 })
