11f5f77d7ed1341519885d9f02101fafb93a4bf4
midn8crow <231871152+midn8crow@users.noreply.github.com>
2026-07-29 22:35:46 +0600

    Cleanup + updated 2026-07-29_22:35

--- .config/hypr/modules/binds.lua
+++ .config/hypr/modules/binds.lua
@@ -264,4 +264,4 @@ hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
 -- Mechanical keypress sounds (WayClick) toggle
-hl.bind("CTRL + X", hl.dsp.exec_cmd(wayclick))
+hl.bind("CTRL + SHIFT + X", hl.dsp.exec_cmd(wayclick))
