001a13864209f57c36765879869123955dfc2639
midn8crow <231871152+midn8crow@users.noreply.github.com>
2026-07-30 11:29:02 +0600

    Cleanup + updated 2026-07-30_11:29

--- .config/hypr/modules/workspace_overview.lua
+++ .config/hypr/modules/workspace_overview.lua
@@ -2,22 +2,26 @@
 ---- WORKSPACE OVERVIEW ----
 ---------------------------
 
--- Hyprexpo workspace overview plugin config + keybind
+-- ScrollOverview plugin config + keybind
 hl.config({
     plugin = {
-        hyprexpo = {
-            columns = 3,
-            gaps_in = 5,
-            gaps_out = 0,
-            bg_col = "rgb(1E1E2E)",
-            workspace_method = "center current",
+        scrolloverview = {
             gesture_distance = 300,
-            cancel_key = "escape",
-            show_cursor = 1,
+            scale = 0.5,
+            workspace_gap = 100,
+            layout = "vertical",
+            wallpaper = 0,
+            blur = false,
+            shadow = {
+                enabled = false,
+                range = 50,
+                render_power = 3,
+                color = 0xee1a1a1a,
+            },
         },
     },
 })
 
 hl.bind("SUPER + O", function()
-    hl.plugin.hyprexpo.expo("toggle")
+    hl.plugin.scrolloverview.overview("toggle")
 end)
