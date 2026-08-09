d95bbb9f06555c5c273334bc555a38333781d01d
midn8crow <231871152+midn8crow@users.noreply.github.com>
2026-08-09 14:05:39 +0600

    Cleanup + updated 2026-08-09_14:05

--- scripts/ORCHESTRA.sh
+++ scripts/ORCHESTRA.sh
@@ -561,6 +561,9 @@ install_core_packages() {
         "python-curl-cffi-git"
         "scdoc"
         "unzip"
+        "python-curl-cffi-git"
+        "scdoc"
+        "unzip"
     )
     
     # Clipboard
